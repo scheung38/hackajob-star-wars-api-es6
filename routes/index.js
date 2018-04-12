84d7f220bd23384c7d8fb925ba5f6df53a3e6271
sebastian_cheung@yahoo.com <miriam_z@icloud.com>
2018-04-12 23:37:29 +0200

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const getContent = async function (url) {
+// getContent('https://challenges.hackajob.co/swapi/api/films')
+// https://challenges.hackajob.co/swapi/api/films/?format=json
+// https://challenges.hackajob.co/swapi/api/people/?format=json
 
-    // filmsAndCharacters = null
-    // return filmsAndCharacters;
+const getContent = async function (url) {
 
     return await new Promise((resolve, reject) => {
         const lib = url.startsWith('https') ? require('https') : require('http');
@@ -13,51 +14,40 @@ const getContent = async function (url) {
                 reject(new Error('Failed to load page, status code ' + response.statusCode));
             }
             const body = [];
-
             response.on('data', (chunk) => body.push(chunk));
-
             response.on('end', () => resolve(body.join('')));
         });
-
         request.on('error', (err) => reject(err))
     })
 };
 
+function run(film_title, character) {
 
-// getContent('https://challenges.hackajob.co/swapi/api/films')
-// https://challenges.hackajob.co/swapi/api/films/?format=json
-// https://challenges.hackajob.co/swapi/api/people/?format=json
-async function run(film_title, character) {
-
-    // const descriptor1 = Object.getOwnPropertyDescriptor(object1, film);
-
-    let res1 = await getContent('https://challenges.hackajob.co/swapi/api/films/?format=json')
-    // .then(films => console.log("this is films html :", Object.getOwnPropertyDescriptor(films['results'][0]['characters'], film_title).value))
+    let res1 = getContent('https://challenges.hackajob.co/swapi/api/films/?format=json')
         .then(films => JSON.parse(films))
-        .then(res => console.log("typeof :", typeof(res['results'][0])))
-        // .then(res => res['results'].forEach(function (res) {
-        //     if (['results'][0]['title'] === film_title) {
-        //         console.log("filtered output ", res['results'][0]['characters'])
-        //     }
-        //
-        // }))
-        // // .then(res => console.log("parsed characters in this film :", res))
-            .catch(err => console.error(err))
+        .then(res_films => res_films['results'].filter(function (el) {
+            if (el['title'] === film_title) {
+                return console.log(el['characters'])
+            }
+        }))
+        .catch(err => console.error(err));
 
-    let res2 = await getContent('https://challenges.hackajob.co/swapi/api/people/?format=json')
+    let res2 = getContent('https://challenges.hackajob.co/swapi/api/people/?format=json')
         .then(people => JSON.parse(people))
-        .then(res => res['results'].forEach(res['results'][0]['name'] === character ? res['results'][0]['films'] : null))
-        .then(res => console.log("parsed film this character appeared in :", res))
-        .catch(err => console.error(err))
+        .then(res_people => res_people['results'].filter(function (el) {
+            if (el['name'] === character) {
+                return console.log(el['films'])
+            }
+        }))
+        .catch(err => console.error(err));
 
-    return await Promise.all([res1, res2]);
-//
+    return Promise.all([res1, res2]);
 }
 
-film_list = ["A New Hope", "The Empire Strikes Back", "The Force Awakens"];
-character_list = ["Luke Skywalker", "Raymus Antilles", "Poggle the Lesser"]
-console.log(run(film_list[1], character_list[0]));
+film_list = ["A New Hope", "The Empire Strikes Back", "Return of the Jedi"];
+character_list = ["Luke Skywalker", "C-3PO", "R2-D2", "Darth Vader", "Leia Organa", "Raymus Antilles", "Poggle the Lesser"];
 
+console.log(run(film_list[2], character_list[4]));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
